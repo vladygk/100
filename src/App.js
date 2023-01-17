c379fe7d6ef7c00259eab7f06d7909d008713b1c
Vladislav Kotsev <vladygk8@gmail.com>
2023-01-17 08:12:30 +0200

    add: inline style

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="wrapper">
-        <h3 className="mealHeading">Sushi</h3>
-        <div className="description">I prefer steak than sushi.</div>
-        <div className="price">$9.99</div>
+      <div
+        style={{
+          display: "flex",
+          "justify-content": "space-between",
+          margin: "1rem",
+          "padding-bottom": "1rem",
+          "border-bottom": "1px solid #ccc",
+        }}
+      >
+        <h3 style={{ margin: "0 0 0.25rem 0" }}>Sushi</h3>
+        <div style={{ "font-style": "italic" }}>I prefer steak than sushi.</div>
+        <div
+          style={{
+            "margin-top": "0.25rem",
+            "font-weight": "bold",
+            color: "#ad5502",
+            "font-size": "1.25rem",
+          }}
+        >
+          $9.99
+        </div>
       </div>
     </div>
   );
